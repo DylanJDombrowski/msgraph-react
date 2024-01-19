be94b007b553eaefaa76a346d9643896714fa140
DioBytes <dyland601@gmail.com>
2024-01-19 14:40:10 -0500

    first commit

---
+++ src/Config.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const config = {
+  appId: '3d698c5f-a7fa-4b12-b351-82998f560976',
+  redirectUri: 'http://localhost:8080/MyApp',
+  scopes: [
+    'user.read',
+    'mailboxsettings.read',
+    'calendars.readwrite'
+  ]
+};
+
+export default config;
